969c09843636ba656ad4458d22933516b647c934
paulo.resende.rbmweb <paulo.resende@rbmweb.com.br>
2022-09-20 16:35:28 -0300

    feat: adicionando filtro de pesquisa

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+export interface InputPropsInterface
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export default function Input(){
-    return (
-        <input type="text"/>
-    );
+export default function Input({
+  children,
+  className,
+  ...rest
+}: InputPropsInterface) {
+  return (
+    <input {...rest} className={className}>
+      {children}
+    </input>
+  );
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react"
 import { Col, Row } from "react-bootstrap"
+import Input from "../components/input"
 import { StoreItem } from "../components/StoreItem"
 import storeItems from "../data/items.json"
 
 export function Store() {
+  const [car, setcars] = useState(storeItems);
+  const handleOnChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchInputValue = event.target.value; 
+    const filterTypeValue = "name";
+    const newCar = storeItems.filter((car) => car[filterTypeValue].toLowerCase().includes(searchInputValue));
+    setcars(newCar);
+  };
   return (
-    <>
-      <h1 className="titulo-pagina">Carros</h1>
+    <> 
+      <div className="d-flex justify-content-between">
+        <h1 className="titulo-pagina">Carros</h1> 
+        <div className="d-flex justify-content-center p-3">
+          <Input
+            type="text"
+            placeholder="Procure pelo carro desejado"
+            onChange={handleOnChangeInput}
+            style={{
+              outline: "none",
+            }}
+          />
+        </div> 
+      </div>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {storeItems.map(item => (
+        {car.map(item => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
